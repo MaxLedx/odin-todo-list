63eada1dfe0782f659985bf30ae163b02b3689e7
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2025-05-20 18:29:01 +0200

    refactor: simplify models

--- src/project.js
+++ src/project.js
@@ -1,53 +1,9 @@
 export class Project {
-    #id = crypto.randomUUID();
-    #title;
-    #description;
-    #color;
-    #todos = [];
-
-    constructor(title, description, color) {
-        this.#title = title;
-        this.#description = description;
-        this.#color = color;
-    }
-
-    get id() {
-        return this.#id;
-    }
-
-    get title() {
-        return this.#title;
-    }
-
-    set title(value) {
-        this.#title = value;
-    }
-
-    get description() {
-        return this.#description;
-    }
-
-    set description(value) {
-        this.#description = value;
-    }
-
-    get color() {
-        return this.#color;
-    }
-
-    set color(value) {
-        this.#color = value;
-    }
-
-    addTodo(todo) {
-        this.#todos.push(todo);
-    }
-
-    getTodo(id) {
-        return this.#todos.find(t => t.id === id);
-    }
-
-    deleteTodo(id) {
-        this.#todos = this.#todos.filter(t => t.id !== id);
+    constructor({ title, description, color }) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.color = color;
+        this.todos = [];
     }
 }
